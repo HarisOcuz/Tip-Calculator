b7b86c2768bac28ee92fefa125229624ef9ca129
Haris Ocuz <hariso94@outlook.com>
2025-10-16 12:15:02 +0200

    App vollfunktionsfähig - CSS muss noch gemacht werden

--- src/App.js
+++ src/App.js
@@ -1,80 +1,46 @@
 import { useState } from "react";
 import "./index.css";
 
-// const faqs = [
-//   {
-//     title: "Where are these chairs assembled?",
-//     text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
-//   },
-//   {
-//     title: "How long do I have to return my chair?",
-//     text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
-//   },
-//   {
-//     title: "Do you ship to countries outside the EU?",
-//     text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
-//   },
-// ];
-
-// export default function App() {
-//   return (
-//     <div>
-//       <Accordion data={faqs} />
-//     </div>
-//   );
-// }
-
-// function Accordion({ data }) {
-//   const [curOpen, setCurOpen] = useState(null);
-
-//   return (
-//     <div className="accordion">
-//       {data.map((el, i) => (
-//         <AccordionItem
-//           curOpen={curOpen}
-//           onOpen={setCurOpen}
-//           title={el.title}
-//           num={i}
-//           key={el.title}
-//         >
-//           {el.text}
-//         </AccordionItem>
-//       ))}
-//     </div>
-//   );
-// }
-// function AccordionItem({ num, title, curOpen, onOpen, children }) {
-//   const isActive = num === curOpen;
-//   function handleIsActive() {
-//     onOpen(isActive ? null : num);
-//     // if (curOpen === num) onOpen(null);
-//   }
-
-//   return (
-//     <div className={`item ${isActive ? "open" : ""}`} onClick={handleIsActive}>
-//       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
-//       <p className="text">{title}</p>
-//       <p className="icon">{isActive ? "-" : "+"}</p>
-//       {isActive ? <div className="content-box">{children}</div> : ""}
-//     </div>
-//   );
-// }
-
 const options = [
-  "Nothing special (0%)",
-  "It was good (10%)",
-  "Absolutely amazing (20%)",
+  { title: "Nothing special (0%)", id: 0 },
+  { title: "It was okay (5%)", id: 5 },
+  { title: "It was good (10%)", id: 10 },
+  { title: "Absolutely amazing (20%)", id: 20 },
 ];
 
 export default function App() {
   const [amount, setAmount] = useState(0);
+  const [curTip, setCurTip] = useState(0);
+  const [curFriendTip, setCurFriendTip] = useState(0);
+
+  let tipTotal = (curTip + curFriendTip) / 100;
+
+  console.log(tipTotal);
+
+  function handleCurFriendTip(e) {
+    setCurFriendTip(Number(e.target.value));
+  }
+
+  function handleCurTip(e) {
+    setCurTip(Number(e.target.value));
+  }
 
   return (
-    <div>
+    <div className="container">
       <BillAmount amount={amount} onAmount={setAmount} />
-      <TipCalcMe />
-      <TipCalcFriend />
-      <Output amount={amount} onAmount={setAmount} />
+      <TipCalcMe onCurTip={handleCurTip} curTip={curTip} />
+      <TipCalcFriend
+        onCurFriendTip={handleCurFriendTip}
+        curFriendTip={curFriendTip}
+      />
+      <Output
+        amount={amount}
+        onAmount={setAmount}
+        curTip={curTip}
+        setCurTip={setCurTip}
+        setCurFriendTip={setCurFriendTip}
+        tipTotal={tipTotal}
+      />
     </div>
   );
 }
@@ -92,22 +58,22 @@ function BillAmount({ amount, onAmount }) {
   );
 }
 
-function TipCalcMe() {
+function TipCalcMe({ onCurTip, curTip }) {
   return (
     <div className="tip-con">
       <p>How did you like the service ?</p>
-      <select>
+      <select value={curTip} onChange={onCurTip}>
         <Options />
       </select>
     </div>
   );
 }
 
-function TipCalcFriend() {
+function TipCalcFriend({ curFriendTip, onCurFriendTip }) {
   return (
     <div className="tip-con">
       <p>How did your friend like the service ?</p>
-      <select>
+      <select value={curFriendTip} onChange={onCurFriendTip}>
         <Options />
       </select>
     </div>
@@ -115,20 +81,22 @@ function TipCalcFriend() {
 }
 
 function Options() {
-  return options.map((opt) => <option>{opt}</option>);
+  return options.map((opt) => <option value={opt.id}>{opt.title}</option>);
 }
 
-function Output({ amount, onAmount }) {
+function Output({ amount, onAmount, setCurTip, tipTotal, setCurFriendTip }) {
   function BtnReset() {
     onAmount(0);
+    setCurFriendTip("");
+    setCurTip("");
   }
 
   return (
     <div>
-      <h3>{`Your pay ${amount} (${amount} + TIP) `}</h3>
+      <h3>{`You pay ${amount + amount * tipTotal}€ (${amount}€ + ${
+        amount * tipTotal
+      }€)  `}</h3>
       <button onClick={BtnReset}>Reset</button>
     </div>
   );
 }
-
-// Total bil radi moze se unijeti iznos, drop-down opcije takodjer rade, value iz bill se prenosi skroz do outputa. I reset dugme radi, vraca bill na 0
